feef3035bf7b5a4e18b43fe319632739a3386956
jfx21 <quba.adamczyk@icloud.com>
2024-11-03 15:28:12 +0100

    Created OrderServiceApplication

---
+++ OrderService/src/main/kotlin/org/jfx/orderservice/OrderServiceApplication.kt
@@ -0,0 +1,10 @@
+package org.jfx.orderservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+open class OrderServiceApplication
+fun main(args: Array<String>) {
+    runApplication<OrderServiceApplication>(*args)
+}
