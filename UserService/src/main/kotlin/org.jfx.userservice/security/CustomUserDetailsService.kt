5e99dc60a2be0e88949e1afcc24e7c16baf5a982
jfx21 <quba.adamczyk@icloud.com>
2024-11-04 23:21:41 +0100

    Created CustomUserDetailsService

---
+++ UserService/src/main/kotlin/org.jfx.userservice/security/CustomUserDetailsService.kt
@@ -0,0 +1,22 @@
+package org.jfx.userservice.security
+
+import org.jfx.userservice.repository.UserRepository
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+
+//TODO
+@Service
+class CustomUserDetailsService(
+    private val userRepository: UserRepository
+) : UserDetailsService {
+
+    override fun loadUserByUsername(username: String): UserDetails {
+        val user = userRepository.findByUsername(username)
+            ?: throw UsernameNotFoundException("User not found with username: $username")
+        return org.springframework.security.core.userdetails.User(
+            user.username, user.password, user.authorities
+        )
+    }
+}
