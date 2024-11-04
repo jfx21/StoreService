9e370f5aa5bb033a0f32b413ed9a5448c818d5cd
jfx21 <quba.adamczyk@icloud.com>
2024-11-04 23:17:33 +0100

    SecurityConfig improvement

--- UserService/src/main/kotlin/org.jfx.userservice/security/SecurityConfig.kt
+++ UserService/src/main/kotlin/org.jfx.userservice/security/SecurityConfig.kt
@@ -1,5 +1,6 @@
 package org.jfx.userservice.security
 
+import org.jfx.userservice.security.jwt.JwtTokenFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.AuthenticationManager
@@ -7,29 +8,32 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
 open class SecurityConfig(
+    private val customUserDetailsService: CustomUserDetailsService,
     private val jwtTokenFilter: JwtTokenFilter
 ) {
-
     @Bean
     open fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http
-            .csrf { csrf -> csrf.disable() } // Disable CSRF for JWT authentication
+            .csrf { csrf -> csrf.disable() }
             .sessionManagement { sessionManagement ->
                 sessionManagement
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Stateless session
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             }
-            .authorizeHttpRequests { authz ->
-                authz
-                    .requestMatchers("/api/users/**").permitAll() // Allow public access to authentication endpoints
-                    .anyRequest().authenticated() // Require authentication for all other endpoints
+            .authorizeHttpRequests { auths ->
+                auths
+                    .requestMatchers("/api/auth/**").permitAll()
+                    .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                    .anyRequest().authenticated()
             }
-            .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter::class.java) // Add the JWT filter
+            .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter::class.java)
 
         return http.build()
     }
@@ -38,5 +42,9 @@ open class SecurityConfig(
     @Bean
     open fun authenticationManager(authenticationConfiguration: AuthenticationConfiguration): AuthenticationManager {
         return authenticationConfiguration.authenticationManager
+    }
+    @Bean
+    open fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder() //check if it's the secure version
     }
 }
