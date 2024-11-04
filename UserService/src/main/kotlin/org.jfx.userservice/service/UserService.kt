f7f37be034f65760f64dac213ed1afe5dfb01281
jfx21 <quba.adamczyk@icloud.com>
2024-11-04 23:21:10 +0100

    UserService provided new methods

--- UserService/src/main/kotlin/org.jfx.userservice/service/UserService.kt
+++ UserService/src/main/kotlin/org.jfx.userservice/service/UserService.kt
@@ -1,8 +1,12 @@
 package org.jfx.userservice.service
 
 import org.jfx.userservice.model.User
+import org.jfx.userservice.model.UserRegistrationDto
 import org.jfx.userservice.repository.RoleRepository
 import org.jfx.userservice.repository.UserRepository
+import org.springframework.context.annotation.Bean
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
@@ -13,13 +17,16 @@ class UserService(
     private val passwordEncoder: PasswordEncoder
 ) {
 
-    fun registerUser(username: String, password: String) {
+    fun registerUser(registrationDto: UserRegistrationDto) {
+        val encodedPassword = passwordEncoder.encode(registrationDto.password)
+
         val userRole = roleRepository.findByName("USER")
             ?: throw IllegalArgumentException("Default role not found")
 
         val user = User(
-            username = username,
-            password = passwordEncoder.encode(password),
+            username = registrationDto.username,
+            email = registrationDto.email,
+            password = encodedPassword,
             roles = setOf(userRole)
         )
 
@@ -28,13 +35,19 @@ class UserService(
 
     fun assignRoleToUser(username: String, roleName: String) {
         val user = userRepository.findByUsername(username)
-            ?: throw IllegalArgumentException("User not found")
+            ?: throw IllegalArgumentException("User not found") //TODO custom exception
 
         val role = roleRepository.findByName(roleName)
-            ?: throw IllegalArgumentException("Role not found")
+            ?: throw IllegalArgumentException("Role not found") //TODO custom exception
 
         user.roles += role
         userRepository.save(user)
     }
+    fun findByUsername(username: String) = userRepository.findByUsername(username)
+    fun getCurrentUser(): User {
+        val username = SecurityContextHolder.getContext().authentication.name
+        return userRepository.findByUsername(username) ?: throw Exception("User not found") //TODO custom exception
+    }
+
 }
 
