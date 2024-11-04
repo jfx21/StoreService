718bac6b576d012f6cbf5ffaa29a372e0a9dd2f9
jfx21 <quba.adamczyk@icloud.com>
2024-11-04 20:48:28 +0100

    Created role entity

---
+++ UserService/src/main/kotlin/org.jfx.userservice/model/Role.kt
@@ -0,0 +1,14 @@
+package org.jfx.userservice.model
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
+data class Role(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id:Long? = null,
+    var name: String = "" //name for ROLE, USER, ADMIN,
+)
