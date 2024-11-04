80d751cbbf0791751bc12eac16d02f61765417c2
jfx21 <quba.adamczyk@icloud.com>
2024-11-04 20:50:22 +0100

    Improved User entity by adding UserDetails from Spring

--- UserService/src/main/kotlin/org.jfx.userservice/model/User.kt
+++ UserService/src/main/kotlin/org.jfx.userservice/model/User.kt
@@ -1,11 +1,12 @@
 package org.jfx.userservice.model
 
-import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.persistence.*
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Size
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
 
 
 @Entity
@@ -26,6 +27,32 @@ data class User(
 
     @field:Size(min = 6)
     @JsonProperty("password")
-    val password: String
-)
+    val password: String,
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+    name = "user_roles",
+    joinColumns = [JoinColumn(name = "user_id")],
+    inverseJoinColumns = [JoinColumn(name = "role_id")])
+    var roles: Set<Role> = emptySet()
+) : UserDetails{
+    override fun getAuthorities(): Collection<GrantedAuthority> {
+        return roles.map{ GrantedAuthority { it.name }}
+    }
+
+    override fun getPassword(): String {
+        return password //should be used with BCRYPT, remember about last OKTA accident
+    }
+
+    override fun getUsername(): String {
+       return username
+    }
+    override fun isAccountNonExpired(): Boolean = true
+
+    override fun isAccountNonLocked(): Boolean = true
+
+    override fun isCredentialsNonExpired(): Boolean = true
+
+    override fun isEnabled(): Boolean = true
+}
 
