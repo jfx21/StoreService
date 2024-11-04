efd88a63a9058f81b604315a76b8809322d50a41
jfx21 <quba.adamczyk@icloud.com>
2024-11-04 20:03:38 +0100

    Created main app in ProductService

---
+++ ProductService/src/main/kotlin/org/jfx/productservice/ProductServiceApplication.kt
@@ -0,0 +1,11 @@
+package org.jfx.productservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+open class ProductServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProductServiceApplication>(*args)
+}
