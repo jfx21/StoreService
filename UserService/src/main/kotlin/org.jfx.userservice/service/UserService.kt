0cf6c71737338d8ff21f0ca8e75901a4cce2aaf7
jfx21 <quba.adamczyk@icloud.com>
2024-11-10 18:31:49 +0100

    Added validation to user registration

--- UserService/src/main/kotlin/org.jfx.userservice/service/UserService.kt
+++ UserService/src/main/kotlin/org.jfx.userservice/service/UserService.kt
@@ -1,9 +1,11 @@
 package org.jfx.userservice.service
 
+import org.jfx.userservice.exception.UserDataIsNotValidException
 import org.jfx.userservice.model.Role
 import org.jfx.userservice.model.User
 import org.jfx.userservice.model.dto.UserRegistrationDto
 import org.jfx.userservice.repository.UserRepository
+import org.jfx.userservice.util.JsonUtil
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.security.crypto.password.PasswordEncoder
@@ -12,19 +14,26 @@ import org.springframework.stereotype.Service
 @Service
 open class UserService(
     private val userRepository: UserRepository,
-    private val passwordEncoder: PasswordEncoder
+    private val passwordEncoder: PasswordEncoder,
+    private val userDataValidationService: UserDataValidationService
 ) {
+    private val jsonUtil: JsonUtil = JsonUtil()
     fun registerUser(registrationDto: UserRegistrationDto) {
         val encodedPassword = passwordEncoder.encode(registrationDto.password)
-        val user = User(
-            username = registrationDto.username,
-            email = registrationDto.email,
-            phoneNumber = registrationDto.phoneNumber,
-            password = encodedPassword,
-            roles = setOf(Role.USER)
-        )
+        val validationResult = userDataValidationService.validateUserInputs(registrationDto)
+        if (validationResult.isUserInputCorrect) {
+            val user = User(
+                username = registrationDto.username,
+                email = registrationDto.email,
+                phoneNumber = registrationDto.phoneNumber,
+                password = encodedPassword,
+                roles = setOf(Role.USER)
+            )
 
-        userRepository.save(user)
+            userRepository.save(user)
+        } else {
+            throw UserDataIsNotValidException(jsonUtil.toJson(validationResult))
+        }
     }
 
     fun getCurrentUser(): User {
