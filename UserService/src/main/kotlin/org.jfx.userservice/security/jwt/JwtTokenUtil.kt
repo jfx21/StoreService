5f8e2f0810c7abf2fb1d21a4c41061a179db97a0
jfx21 <quba.adamczyk@icloud.com>
2024-11-10 14:21:54 +0100

    Adjusted JwtTokenUtil with newer dependency version, added exception cases

--- UserService/src/main/kotlin/org.jfx.userservice/security/jwt/JwtTokenUtil.kt
+++ UserService/src/main/kotlin/org.jfx.userservice/security/jwt/JwtTokenUtil.kt
@@ -1,27 +1,27 @@
 package org.jfx.userservice.security.jwt
 
-import io.jsonwebtoken.ExpiredJwtException
-import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.*
 import lombok.extern.slf4j.Slf4j
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.autoconfigure.security.SecurityProperties
 import org.springframework.security.core.Authentication
-import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Component
+import java.security.SignatureException
 import java.util.Date
-import kotlin.math.log
+import javax.crypto.SecretKey
 
 @Component
 @Slf4j
 class JwtTokenUtil(
-    @Value("\${jwt.secret}") private val jwtSecret: String,
     @Value("\${jwt.expiration}") private val jwtExpirationMs: Long
 ) {
     private val logger: Logger = LoggerFactory.getLogger(JwtTokenUtil::class.java)
+
+    private fun getSigningKey(): SecretKey {
+        return Jwts.SIG.HS512.key().build()
+    }
 
     fun generateToken(authentication: Authentication): String {
         val userPrincipal = authentication.principal as UserDetails
@@ -32,26 +32,46 @@ class JwtTokenUtil(
         val roles = userPrincipal.authorities.map { it.authority }
 
         return Jwts.builder()
-            .setSubject(userPrincipal.username)
+            .subject(userPrincipal.username)
             .claim("roles", roles)
-            .setIssuedAt(now)
-            .setExpiration(expiryDate)
-            .signWith(SignatureAlgorithm.HS512, jwtSecret)
+            .issuedAt(now)
+            .expiration(expiryDate)
+            .signWith(getSigningKey())
             .compact()
     }
 
     fun getUsernameFromToken(token: String): String {
-        val claims = Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).body
-        return claims.subject
+        return Jwts.parser().verifyWith(getSigningKey()).build().parseSignedClaims(token).payload.subject
+
     }
 
     fun validateToken(token: String): Boolean {
         try {
-            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token)
+            Jwts.parser().verifyWith(getSigningKey()).build().parseSignedClaims(token)
             return true
-        } catch (ex: ExpiredJwtException) {
-            // Handle exceptions like ExpiredJwtException, UnsupportedJwtException, etc.
-            logger.error("Invalid JWT token: ${ex.message}")
+        } catch (ex: Exception) {
+            when (ex) {
+                is ExpiredJwtException -> {
+                    logger.error("JWT token expired: ${ex.message}")
+                }
+
+                is MalformedJwtException -> {
+                    logger.error("Invalid JWT token: ${ex.message}")
+                }
+
+                is SignatureException -> {
+                    logger.error("Invalid JWT signature: ${ex.message}")
+                }
+
+                is UnsupportedJwtException -> {
+                    logger.error("Unsupported JWT token: ${ex.message}")
+                }
+
+                is IllegalArgumentException -> {
+                    logger.error("JWT claims string is empty: ${ex.message}")
+                }
+
+            }
         }
         return false
     }
