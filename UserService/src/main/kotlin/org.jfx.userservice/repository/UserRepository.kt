be20516bf3ae29bd146a9ebd91a1802c5cf3076d
jfx21 <quba.adamczyk@icloud.com>
2024-11-10 18:31:27 +0100

    Refined queries

--- UserService/src/main/kotlin/org.jfx.userservice/repository/UserRepository.kt
+++ UserService/src/main/kotlin/org.jfx.userservice/repository/UserRepository.kt
@@ -3,11 +3,12 @@ package org.jfx.userservice.repository
 import org.jfx.userservice.model.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository("UserRepository")
 interface UserRepository : JpaRepository<User, Long> {
     fun findByUsername(username: String): User?
     fun findUserById(id: Long): User?
-    fun findByEmail(email: String): User?
-    fun findByPhoneNumber(phoneNumber: String): User?
+    fun findByEmail(email: String): Optional<User?>
+    fun findByPhoneNumber(phoneNumber: String): Optional<User?>
 }
