257a92af98946d1bf9fd3180b2bdf386b33afb73
jfx21 <quba.adamczyk@icloud.com>
2024-11-10 15:04:28 +0100

    Added custom impl of UserDetailsService from spring

---
+++ UserService/src/main/kotlin/org.jfx.userservice/security/UserDetailsServiceImpl.kt
@@ -0,0 +1,24 @@
+package org.jfx.userservice.security
+
+import org.jfx.userservice.model.User
+import org.jfx.userservice.repository.UserRepository
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.transaction.annotation.Transactional
+
+@Configuration
+open class UserDetailsServiceImpl(private val userRepository: UserRepository): UserDetailsService {
+
+    @Transactional
+    override fun loadUserByUsername(username: String) : UserDetails {
+            val user: User = userRepository.findByUsername(username)
+                ?: throw UsernameNotFoundException("User not found: $username")
+        val authorities = user.roles.map{ SimpleGrantedAuthority(it.name) }
+        return org.springframework.security.core.userdetails.User(
+            user.username,user.password, authorities
+        )
+    }
+}
