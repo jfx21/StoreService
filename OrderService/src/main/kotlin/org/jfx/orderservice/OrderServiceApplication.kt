1cd9b46ecd1a3690fc191cb0d98e88bff7da7891
jfx21 <quba.adamczyk@icloud.com>
2024-12-08 20:39:37 +0100

    Added new base package

--- OrderService/src/main/kotlin/org/jfx/orderservice/OrderServiceApplication.kt
+++ OrderService/src/main/kotlin/org/jfx/orderservice/OrderServiceApplication.kt
@@ -3,7 +3,7 @@ package org.jfx.orderservice
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = ["org.jfx.productservice", "org.jfx.orderservice"])
 open class OrderServiceApplication
 fun main(args: Array<String>) {
     runApplication<OrderServiceApplication>(*args)
