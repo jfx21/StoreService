304e3df8f2cd59ccaaa1969a35fb5e556f355304
jfx21 <quba.adamczyk@icloud.com>
2024-12-13 16:47:40 +0100

    Added transactional annotation to a query

--- OrderService/src/main/kotlin/org/jfx/orderservice/OrderServiceApplication.kt
+++ OrderService/src/main/kotlin/org/jfx/orderservice/OrderServiceApplication.kt
@@ -3,8 +3,9 @@ package org.jfx.orderservice
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(scanBasePackages = ["org.jfx.productservice", "org.jfx.orderservice"])
+@SpringBootApplication(scanBasePackages = ["org.jfx.orderservice","org.jfx.productservice"])
 open class OrderServiceApplication
 fun main(args: Array<String>) {
     runApplication<OrderServiceApplication>(*args)
+
 }
--- OrderService/src/main/kotlin/org/jfx/orderservice/service/OrderService.kt
+++ OrderService/src/main/kotlin/org/jfx/orderservice/service/OrderService.kt
@@ -4,11 +4,9 @@ package org.jfx.orderservice.service
 import org.jfx.orderservice.model.Order
 import org.jfx.orderservice.model.request.OrderRequest
 import org.jfx.orderservice.repository.OrderRepository
-import org.jfx.productservice.model.Product
 import org.jfx.productservice.service.ProductService
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import java.time.LocalDateTime
 
 @Service
 open class OrderService(
@@ -17,57 +15,38 @@ open class OrderService(
 ) {
     @Transactional
     open fun createOrder(orderRequest: OrderRequest): Order {
-        // Process the product details and calculate the total price
         val productDetails = orderRequest.products.map { productOrder ->
             val product = productService.getProductByName(productOrder.productName)
                 ?: throw IllegalArgumentException("Product not found: ${productOrder.productName}")
-
-            // Check if the product is available
             if (!product.isAvailable(productOrder.quantity)) {
                 throw IllegalArgumentException("Not enough stock for product: ${product.name}")
             }
-
-            // Reduce the stock of the product
             productService.reduceProductStock(product.id!!, productOrder.quantity)
-
-            // Calculate the total price for this product (price * quantity)
             val totalPriceForProduct = product.price.toDouble() * productOrder.quantity
-
-            // Return the product ID and the calculated total price for this product
             product.id to totalPriceForProduct
         }
 
-        // Sum up the total price for all products in the order
         val totalPrice = productDetails.sumOf { it.second }
 
-        // Ensure totalPrice is not null or zero
         if (totalPrice <= 0) {
             throw IllegalArgumentException("Total price must be greater than zero")
         }
-
-        // Create the Order object
         val order = Order(
-            userName = orderRequest.username,
-            productIds = productDetails.map { it.first!! },  // List of product IDs
+            username = orderRequest.username,
+            productIds = productDetails.map { it.first!! },
             address = orderRequest.address,
             phoneNumber = orderRequest.phoneNumber,
-            totalPrice = totalPrice  // Ensure this value is set properly
+            totalPrice = totalPrice
         )
-        println(order)
-
-        // Persist the order to the database
         return orderRepository.save(order)
     }
-
-
-
 
     fun getOrderById(id: Long): Order? {
         return orderRepository.findById(id).orElse(null)
     }
-
-    fun getOrdersByUserName(userName: String): List<Order>? {
-        return orderRepository.findByUserName(userName)
+    @Transactional(readOnly = true)
+    open fun getOrdersByUsername(username: String): List<Order>? {
+        return orderRepository.findByUsername(username)
     }
 
     fun updateOrderStatus(id: Long, newStatus: String): Order {
